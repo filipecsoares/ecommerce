e5b7f27f5b81c15379c8e6d949ec29abe811b0e3
Filipe Soares <filipecarlossoares@gmail.com>
2019-07-04 20:35:51 -0300

    Initial structure and JWT config

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const expressValidator = require('express-validator')
+require('dotenv').config()
+
+const authRouter = require('./routes/auth')
+
+const app = express()
+
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+}).then(() => console.log('DB connected'))
+
+// middlewares
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(expressValidator())
+
+// routes middleware
+app.use('/api', authRouter)
+
+const port = process.env.PORT || 8000
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
