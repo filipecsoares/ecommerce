4d0d5dfe59c49dcc72209e49cfe65f3437a990fb
Filipe Soares <filipecarlossoares@gmail.com>
2019-07-10 15:03:13 -0300

    Category post

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const crypto = require('crypto')
+const uuidv1 = require('uuid/v1')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -6,6 +8,29 @@ const userSchema = new mongoose.Schema({
         trim: true,
         required: true,
         maxlength: 32
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    },
+    hashed_password: {
+        type: String,
+        required: true
+    },
+    about: {
+        type: String,
+        trim: true
+    },
+    salt: String,
+    role: {
+        type: Number,
+        default: 0
+    },
+    history: {
+        type: Array,
+        default: []
     }
 }, { timestamps: true }
 )
