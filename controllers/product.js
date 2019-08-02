c782ed24699c17bb6b595674c5ce0f0f9b6c20bb
Filipe Soares <filipecarlossoares@gmail.com>
2019-08-01 21:42:53 -0300

    update product

--- controllers/product.js
+++ controllers/product.js
@@ -71,3 +71,41 @@ exports.remove = (req, res) => {
         })
     })
 }
+
+exports.update = (req, res) => {
+    let form = new formidable.IncomingForm()
+    form.keepExtensions = true
+    form.parse(req, (err, fields, files) => {
+        if(err) {
+            return res.status(400).json({
+                error: 'Image could not be uploaded'
+            })
+        }
+        // check all fields
+        const { name, description, price, category, quantity, shipping } = fields
+        if(!name || !description || !price || !category || !quantity || !shipping) {
+            return res.status(400).json({
+                error: 'All fields are required'
+            })
+        }
+        let product = req.product
+        product = _.extend(product, fields)
+        if(files.photo) {
+            if(files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: 'Image should be less than 1mb in size'
+                })
+            }
+            product.photo.data = fs.readFileSync(files.photo.path)
+            product.photo.contentType = files.photo.type
+        }
+        product.save((error, result) => {
+            if(error) {
+                return res.status(400).json({
+                    error: errorHandler(error)
+                })
+            }
+            res.json(result)
+        })
+    })
+}
