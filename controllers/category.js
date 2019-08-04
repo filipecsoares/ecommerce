b2d5d5f9dc30f3b45078caf67af002c08472f1cf
Filipe Soares <filipecarlossoares@gmail.com>
2019-08-04 19:34:29 -0300

    Category CRUD methods

--- controllers/category.js
+++ controllers/category.js
@@ -1,6 +1,17 @@
 const Category = require('../models/category')
 const { errorHandler } = require('../helpers/dbErrorHandler')
 
+exports.categoryById = (req, res, next, id) => {
+    Category.findById(id).exec((err, category) => {
+        if(err || !category) {
+            res.status(400).json({
+                error: 'Category does not exist'
+            })
+        }
+        req.category = category
+        next()
+    })
+}
 exports.create = (req, res) => {
     const category = new Category(req.body)
 
@@ -10,6 +21,48 @@ exports.create = (req, res) => {
                 error: errorHandler(err)
             })
         }
-        return res.json({data})
+        return res.json(data)
+    })
+}
+
+exports.read = (req, res) => {
+    return res.json(req.category)
+}
+
+exports.update = (req, res) => {
+    const category = req.category
+    category.name = req.body.name
+    category.save((err, data) => {
+        if(err) {
+            res.status(400).json({
+                error: errorHandler(err)
+            })
+        }
+        return res.json(data)
+    })
+}
+
+exports.remove = (req, res) => {
+    const category = req.category
+    category.remove((err, data) => {
+        if(err) {
+            res.status(400).json({
+                error: errorHandler(err)
+            })
+        }
+        return res.json({
+            message: 'Category deleted'
+        })
+    })
+}
+
+exports.list = (req, res) => {
+    Category.find().exec((err, data) => {
+        if(err) {
+            res.status(400).json({
+                error: errorHandler(err)
+            })
+        }
+        return res.json(data)
     })
 }
