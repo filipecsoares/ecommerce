01e28017d142bf89232a1114627ff221cc36d0ad
Filipe Soares <filipecarlossoares@gmail.com>
2019-11-07 21:31:14 -0300

    Adjust on Update product

--- controllers/product.js
+++ controllers/product.js
@@ -84,12 +84,13 @@ exports.update = (req, res) => {
             })
         }
         // check all fields
-        const { name, description, price, category, quantity, shipping } = fields
-        if(!name || !description || !price || !category || !quantity || !shipping) {
-            return res.status(400).json({
-                error: 'All fields are required'
-            })
-        }
+        // const { name, description, price, category, quantity, shipping } = fields
+        // console.log(fields)
+        // if(!name || !description || !price || !category || !quantity || !shipping) {
+        //     return res.status(400).json({
+        //         error: 'All fields are required'
+        //     })
+        // }
         let product = req.product
         product = _.extend(product, fields)
         if(files.photo) {
